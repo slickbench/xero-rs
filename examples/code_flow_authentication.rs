ce8039e2ecf2ec5212f6c7f8ee227a6a19fd0fd8
Oscar Rainford <oscar@fourbs.com.au>
2021-07-27 20:18:56 +1000

    feat: implement code flow authorization support.

---
+++ examples/code_flow_authentication.rs
@@ -0,0 +1,68 @@
+#[macro_use]
+extern crate tracing;
+
+use std::{convert::Infallible, str::FromStr, sync::Arc, time::Duration};
+
+use anyhow::Result;
+use oauth2::Scope;
+use serde::Deserialize;
+use tokio::sync::Mutex;
+use url::Url;
+use warp::Filter;
+use xero_rs::KeyPair;
+
+lazy_static::lazy_static! {
+    static ref REDIRECT_ARGS: Arc<Mutex<Option<RedirectArgs>>> = Arc::new(Mutex::new(None));
+}
+
+#[derive(Clone, Deserialize)]
+struct RedirectArgs {
+    code: String,
+    state: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter("debug,xero_rs=trace")
+        .init();
+
+    tokio::spawn(async move {
+        let redirect = warp::get()
+            .and(warp::path("redirect"))
+            .and(warp::query::<RedirectArgs>())
+            .and_then(|args| async {
+                *REDIRECT_ARGS.lock().await = Some(args);
+                Ok("success".to_string()) as Result<String, Infallible>
+            });
+        warp::serve(redirect).run(([127, 0, 0, 1], 4000)).await
+    });
+
+    let key_pair = KeyPair::from_env();
+    let redirect_url = Url::from_str("http://localhost:4000/redirect")?;
+    let (authorize_url, csrf_token) = xero_rs::Client::authorize_url(
+        key_pair.clone(),
+        redirect_url.clone(),
+        vec![Scope::new("openid".to_string())],
+    );
+    info!("Sign in to Xero: {}", authorize_url.to_string());
+
+    info!("Waiting for redirect URL to be hit...");
+    let RedirectArgs { code, state } = loop {
+        tokio::time::sleep(Duration::from_millis(10)).await;
+        if let Some(args) = REDIRECT_ARGS.try_lock().ok().and_then(|c| c.clone()) {
+            break args;
+        }
+    };
+    assert_eq!(&state.expect("missing state"), csrf_token.secret());
+
+    let client = xero_rs::Client::from_authorization_code(key_pair, redirect_url, code).await?;
+
+    let connections = xero_rs::connection::list(&client).await?;
+    info!("found client connections: {:#?}", connections);
+
+    let invoices = xero_rs::invoice::list(&client).await?;
+    info!("found invoices: {:#?}", invoices);
+
+    Ok(())
+}
--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,12 @@
 use core::fmt;
+use std::borrow::Cow;
 
-use oauth2::TokenResponse;
+use oauth2::{AuthorizationCode, CsrfToken, TokenResponse};
 use reqwest::{header, IntoUrl, Method, RequestBuilder, StatusCode};
 use serde::{de::DeserializeOwned, Serialize};
 use url::Url;
+
+pub use oauth2::Scope;
 
 use crate::error::{self, Error, Result};
 use crate::oauth::{KeyPair, OAuthClient};
@@ -22,13 +25,51 @@ pub struct Client {
 }
 
 impl Client {
+    #[instrument]
+    fn build_http_client(access_token: &oauth2::AccessToken) -> reqwest::Client {
+        let mut headers = header::HeaderMap::new();
+        headers.append(
+            "Authorization",
+            header::HeaderValue::from_str(&format!("Bearer {}", access_token.secret())).unwrap(),
+        );
+        reqwest::Client::builder()
+            .default_headers(headers)
+            .build()
+            .unwrap()
+    }
+
+    #[instrument]
+    fn build_oauth_client(key_pair: KeyPair) -> OAuthClient {
+        oauth2::Client::new(
+            key_pair.0,
+            key_pair.1,
+            oauth2::AuthUrl::new(XERO_AUTH_URL.to_string()).unwrap(),
+            Some(oauth2::TokenUrl::new(XERO_TOKEN_URL.to_string()).unwrap()),
+        )
+    }
+
+    /// Generates an authorization URL to use for the code flow authorization method.
+    #[instrument]
+    pub fn authorize_url(
+        key_pair: KeyPair,
+        redirect_url: Url,
+        scopes: Vec<Scope>,
+    ) -> (Url, CsrfToken) {
+        let oauth_client = Self::build_oauth_client(key_pair)
+            .set_redirect_uri(oauth2::RedirectUrl::from_url(redirect_url));
+
+        oauth_client
+            .authorize_url(CsrfToken::new_random)
+            .add_scopes(scopes)
+            .url()
+    }
+
     /// # Errors
     /// Returns an error if the connection can't be made.
-    /// # Panics
     #[instrument]
     pub async fn from_client_credentials(
         key_pair: KeyPair,
-        scopes: Option<Vec<oauth2::Scope>>,
+        scopes: Option<Vec<Scope>>,
     ) -> std::result::Result<
         Self,
         oauth2::RequestTokenError<
@@ -36,13 +77,7 @@ impl Client {
             error::OAuth2ErrorResponse,
         >,
     > {
-        trace!("building oauth2 client");
-        let oauth_client: OAuthClient = oauth2::Client::new(
-            key_pair.0,
-            key_pair.1,
-            oauth2::AuthUrl::new(XERO_AUTH_URL.to_string()).unwrap(),
-            Some(oauth2::TokenUrl::new(XERO_TOKEN_URL.to_string()).unwrap()),
-        );
+        let oauth_client = Self::build_oauth_client(key_pair);
 
         trace!("retrieving access token w/ client credentials grant");
         let token_result = oauth_client
@@ -50,21 +85,43 @@ impl Client {
             .add_scopes(scopes.unwrap_or_default())
             .request_async(oauth2::reqwest::async_http_client)
             .await?;
-        let access_token = token_result.access_token().secret();
-
-        let mut headers = header::HeaderMap::new();
-        headers.append(
-            "Authorization",
-            header::HeaderValue::from_str(&format!("Bearer {}", access_token)).unwrap(),
-        );
-        let http_client = reqwest::Client::builder()
-            .default_headers(headers)
-            .build()
-            .unwrap();
 
         Ok(Self {
             oauth_client,
-            http_client,
+            http_client: Self::build_http_client(token_result.access_token()),
+            redirect_uris: None,
+            grant_type: None,
+            scopes: None,
+            state: None,
+        })
+    }
+
+    /// Creates an authorized client from a code generated in the code flow authorization method.
+    ///
+    /// # Errors
+    /// Returns an error if the connection can't be made.
+    #[instrument]
+    pub async fn from_authorization_code(
+        key_pair: KeyPair,
+        redirect_url: Url,
+        code: String,
+    ) -> std::result::Result<
+        Self,
+        oauth2::RequestTokenError<
+            oauth2::reqwest::Error<reqwest::Error>,
+            error::OAuth2ErrorResponse,
+        >,
+    > {
+        let oauth_client = Self::build_oauth_client(key_pair);
+        let token_result = oauth_client
+            .exchange_code(AuthorizationCode::new(code))
+            .set_redirect_uri(Cow::Owned(oauth2::RedirectUrl::from_url(redirect_url)))
+            .request_async(oauth2::reqwest::async_http_client)
+            .await?;
+
+        Ok(Self {
+            oauth_client,
+            http_client: Self::build_http_client(token_result.access_token()),
             redirect_uris: None,
             grant_type: None,
             scopes: None,
